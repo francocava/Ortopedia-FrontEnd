0c58ac8e1bb3c861f0a42b8815d2d6ca230be437
francocava <francocavallini@hotmail.com>
2020-10-03 12:56:13 -0300

    mejoro el drawer

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,11 +7,12 @@
       app
       clipped
     >
-    
       <v-list>
         <v-list-item>
-      <v-list-item-action @click.stop="miniVariant = !miniVariant">
-            <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
+          <v-list-item-action @click.stop="miniVariant = !miniVariant">
+            <v-icon
+              >mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon
+            >
           </v-list-item-action>
           <v-list-item-content>
             <v-list-item-title v-text="hola" />
@@ -33,52 +34,55 @@
           </v-list-item-content>
         </v-list-item>
 
-
         <v-list-group
-        v-for="item in items"
-        :key="item.title"
-        v-model="item.active"
-        :prepend-icon="item.icon"
-      >
-        <template v-slot:activator>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title"></v-list-item-title>
-          </v-list-item-content>
-        </template>
-
-        <v-list-item
-          v-for="child in item.items"
-          :key="child.title"
-          :to="child.to"
+          v-for="item in items"
+          :key="item.title"
+          v-model="item.active"
+          :prepend-icon="item.icon"
         >
-          <v-list-item-content>
-            <v-list-item-title v-text="child.title"></v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list-group>
+          <template v-slot:activator>
+            <v-list-item-content>
+              <v-list-item-title v-text="item.title"></v-list-item-title>
+            </v-list-item-content>
+          </template>
 
-
+          <v-list-item
+            v-for="child in item.items"
+            :key="child.title"
+            :to="child.to"
+          >
+            <v-list-item-content>
+              <v-list-item-title v-text="child.title"></v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-group>
       </v-list>
+
+
+      <template v-slot:append>
+        <div class="pa-2">
+          <v-btn text color="#616161">
+            <v-icon>mdi-logout</v-icon>
+            Salir
+            </v-btn>
+        </div>
+      </template>
     </v-navigation-drawer>
-    <v-app-bar
-      clipped-left
-      fixed
-      app
-    >
+    <v-app-bar clipped-left fixed app>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-toolbar-title v-text="title"/>
+      <v-toolbar-title v-text="title" />
       <v-spacer />
-      <v-icon>mdi-account-circle</v-icon>
+
+      <v-avatar>
+        <v-icon large> mdi-account-circle-outline </v-icon>
+      </v-avatar>
     </v-app-bar>
     <v-main>
       <v-container>
         <nuxt />
-      </v-container> 
+      </v-container>
     </v-main>
-    <v-footer
-      :absolute="!fixed"
-      app
-    >
+    <v-footer :absolute="!fixed" app>
       <span>&copy; {{ new Date().getFullYear() }}</span>
     </v-footer>
   </v-app>
@@ -86,69 +90,60 @@
 
 <script>
 export default {
-  data () {
+  data() {
     return {
       drawer: true,
       itemsSimple: [
         {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/'
+          //icon: 'mdi-file-powerpoint-box-outline',
+          icon: 'mdi-format-paragraph',
+          title: 'Pedidos',
+          to: '/',
         },
         {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
+          icon: 'mdi-clipboard-text-outline',
+          title: 'Informes',
+          to: '/inspire',
         },
-        /* {
-          icon: 'mdi-plus-outline',
-          title: 'Nuevo',
-          items: [
-            {title: 'Cliente',to: '/cliente'},
-            {title: 'Sucursal',to: '/sucu'},
-            {title: 'Obra Social',to: '/obs'},
-          ],
-          to: '/'
-        },
-        {
-          icon: 'mdi-file-outline',
-          title: 'Ver',
-          items: [
-            {title: 'Pedidos'},
-            {title: 'Clientes'},
-            {title: 'Pagos'},
-
-          ],
-          to: '/prueba'
-        } */
       ],
       items: [
         {
           icon: 'mdi-plus-outline',
           title: 'Nuevo',
           items: [
-            {title: 'Cliente', to: '/cliente'},
-            {title: 'Sucursal', to: '/sucu'},
-            {title: 'Obra Social', to: '/obs'},
+            { title: 'Cliente', to: '/cliente' },
+            { title: 'Sucursal', to: '/sucursal' },
+            { title: 'Obra social', to: '/obraSocial' },
+            { title: 'Producto', to: '' },
+            { title: 'Accesorio', to: '' },
+            { title: 'Pago', to: '' },
+            { title: 'Cobro', to: '' },
+            { title: 'Proveedor', to: '' },
+            { title: 'Forma de pago', to: '' },
+            { title: 'Empleado', to: '' },
           ],
-          to: '/'
         },
         {
           icon: 'mdi-file-outline',
           title: 'Ver',
           items: [
-            {title: 'Pedidos', to: '/prueba'},
-            {title: 'Clientes', to: '/prueba1'},
-            {title: 'Pagos', to: '/prueba2'},
-
+            { title: 'Pedidos', to: '/pedidos' },
+            { title: 'Facturas', to: '' },
+            { title: 'Pagos', to: '/prueba2' },
+            { title: 'Cobros', to: '' },
+            { title: 'Clientes', to: '/prueba1' },
+            { title: 'Productos', to: '' },
+            { title: 'Accesorios', to: '' },
+            { title: 'Obras Sociales', to: '' },
+            { title: 'Sucursales', to: '' },
+            { title: 'Proveedores', to: '' },
           ],
-          to: '/'
-        }
+        },
       ],
       miniVariant: false,
       right: true,
-      title: 'Ortopedia'
+      title: 'Ortopedia',
     }
-  }
+  },
 }
 </script>
--- pages/cliente.vue
+++ pages/cliente.vue
@@ -1,94 +1,79 @@
 <template>
   <v-layout>
     <v-card>
-       <v-col class="d-flex align-center justify-md-center">
-      <div class="text-center">
-      <v-card-title>Ingresar Cliente Nuevo</v-card-title>
-      <v-form ref="form" v-model="valid" lazy-validation>
-        <v-text-field
-          v-model="nombre"
-          :rules="nombreRules"
-          label="Nombre"
-          required
-        ></v-text-field>
+      <v-col class="d-flex align-center justify-md-center">
+        <div class="text-center">
+          <v-card-title>Ingresar Cliente Nuevo</v-card-title>
+          <v-form ref="form" v-model="valid" lazy-validation>
+            <v-text-field
+              v-model="nombre"
+              :rules="nombreRules"
+              label="Nombre"
+              required
+            ></v-text-field>
 
-        <v-text-field
-          v-model="apellido"
-          :rules="apellidoRules"
-          label="Apellido"
-          required
-        ></v-text-field>
+            <v-text-field
+              v-model="apellido"
+              :rules="apellidoRules"
+              label="Apellido"
+              required
+            ></v-text-field>
 
-        <v-text-field
-          type="number"
-          v-model="dni"
-          :rules="dniRules"
-          label="DNI"
-          required
-        ></v-text-field>
+            <v-text-field
+              type="number"
+              v-model="dni"
+              :rules="dniRules"
+              label="DNI"
+              required
+            ></v-text-field>
 
-        <v-select
-          v-model="select"
-          :items="items"
-          :rules="[(v) => !!v || 'Item is required']"
-          label="Obra Social"
-          required
-        ></v-select>
+            <v-select
+              v-model="select"
+              :items="items"
+              :rules="[(v) => !!v || 'Item is required']"
+              label="Obra Social"
+              required
+            ></v-select>
 
-        <v-btn
-          :disabled="!valid"
-          color="success"
-          class="mr-4"
-          @click="validate"
-        >
-          Enviar
-        </v-btn>
-
-        
-      </v-form>
-      </div>
-       </v-col>
+            <v-btn
+              :disabled="!valid"
+              color="success"
+              class="mr-4"
+              @click="validate"
+            >
+              Enviar
+            </v-btn>
+          </v-form>
+        </div>
+      </v-col>
     </v-card>
   </v-layout>
 </template>
 
 <script>
-  export default {
-    data: () => ({
-      valid: true,
-      nombre: '',
-      nombreRules: [
-        v => !!v || 'Falta el nombre del cliente',
-        v => (v && v.length <= 15) || 'Nombre muy largo',
-      ],
-      apellido: '',
-      apellidoRules: [
-        v => !!v || 'Falta el apellido del cliente',
-        v => (v && v.length <= 25) || 'Apellido muy largo',
-      ],
-      dni:'',
-      dniRules: [
-        v => !!v || 'Falta el DNI del cliente',
-      ],
-      select: null,
-      items: [
-        'Osecac',
-        'Pami',
-        'Osbba',
-        'Swiss Medical',
-      ],
-    }),
+export default {
+  data: () => ({
+    valid: true,
+    nombre: '',
+    nombreRules: [
+      (v) => !!v || 'Falta el nombre del cliente',
+      (v) => (v && v.length <= 15) || 'Nombre muy largo',
+    ],
+    apellido: '',
+    apellidoRules: [
+      (v) => !!v || 'Falta el apellido del cliente',
+      (v) => (v && v.length <= 25) || 'Apellido muy largo',
+    ],
+    dni: '',
+    dniRules: [(v) => !!v || 'Falta el DNI del cliente'],
+    select: null,
+    items: ['Osecac', 'Pami', 'Osbba', 'Swiss Medical'],
+  }),
 
-    methods: {
-      validate () {
-        this.$refs.form.validate()
-      },
-      /* reset () {
-        this.$refs.form.reset()
-      }, */
-      /* resetValidation () {
-        this.$refs.form.resetValidation()
-      }, */
+  methods: {
+    validate() {
+      this.$refs.form.validate()
     },
-  }
+  },
+}
 </script>
