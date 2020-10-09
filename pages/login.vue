0eb4ace089ff4bded5818ab479b65311ff6fa9d9
Agustin Amato <agustin.amato96@gmail.com>
2020-10-09 17:20:07 -0300

    login

---
+++ pages/login.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <v-card>
+      <v-card-title class="headline text-center"> Login </v-card-title>
+      <v-card-text> login form </v-card-text>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'login',
+  data: () => ({
+    //
+  }),
+}
+</script>
