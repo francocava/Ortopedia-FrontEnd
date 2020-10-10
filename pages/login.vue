a7805c411073fd31d9e69a6f65c404ab961c3c8d
francocava <francocavallini@hotmail.com>
2020-10-10 00:43:32 -0300

    agrego login 1.0

--- pages/login.vue
+++ pages/login.vue
@@ -1,10 +1,25 @@
 <template>
-  <div>
     <v-card>
-      <v-card-title class="headline text-center"> Login </v-card-title>
-      <v-card-text> login form </v-card-text>
+      <v-card-title > Inicio </v-card-title>
+      <v-card-text>
+      <v-text-field
+      label="Usuario"
+      outlined>
+
+      </v-text-field>
+      <v-text-field
+      label="Contrasenia"
+      outlined>
+      </v-text-field>
+
+      <v-btn
+          color="success"
+          class="mr-4"
+        >
+          Entrar
+        </v-btn>
+      </v-card-text>
     </v-card>
-  </div>
 </template>
 
 <script>
