d4bef4b7625db7b16d8e43a2acf2b14c1a3ea183
francocava <francocavallini@hotmail.com>
2020-10-17 17:21:59 -0300

    agrego nuevo y ver para proveedor

---
+++ pages/proveedor.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-card>
+    <v-card-title>Proveedor Nuevo</v-card-title>
+
+    <v-card-text>
+      <v-form ref="form" v-model="valid" lazy-validation>
+        <v-text-field
+          v-model="nombre"
+          :rules="nombreRules"
+          label="Nombre"
+          required
+        ></v-text-field>
+
+        <v-btn
+          :disabled="!valid"
+          color="success"
+          class="mr-4"
+          @click="validate"
+        >
+          Enviar
+        </v-btn>
+      </v-form>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    valid: true,
+    nombre: '',
+    nombreRules: [
+      (v) => !!v || 'Falta el nombre del proveedor',
+      (v) => (v && v.length <= 50) || 'Nombre muy largo',
+    ],
+  }),
+
+  methods: {
+    validate() {
+      this.$refs.form.validate()
+    },
+  },
+}
+</script>
