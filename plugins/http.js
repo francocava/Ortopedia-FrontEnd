7bd70f781ab55db63f85d0f5dc905799e95ba26a
Agustin Amato <agustin.amato96@gmail.com>
2020-11-09 23:44:42 -0300

    http 401 status code

--- plugins/http.js
+++ plugins/http.js
@@ -11,6 +11,10 @@ export default function ({ $http }) {
     if (error.statusCode === 500) {
       console.log('Request Error!')
     }
+
+    if (error.statusCode === 401) {
+      console.log('Take me to login page')
+    }
     // Tip: error.response will be undefined if the connection dropped to the server
     // Tip: You can use error.response.data to get response message
     // Tip: You can return an object or Promise as fallback response to avoid rejection
