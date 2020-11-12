b6c1095a5428a0a1bbbb4b9553d6a8a474150e66
Agustin Amato <agustin.amato96@gmail.com>
2020-11-12 18:11:29 -0300

    agrego signo pesos

--- pages/productos.vue
+++ pages/productos.vue
@@ -105,6 +105,11 @@
           </v-dialog>
         </v-toolbar>
       </template>
+
+      <template v-slot:item.precio="{ item }">
+        ${{item.precio}}
+      </template>
+
       <template v-slot:item.actions="{ item }">
         <v-icon small class="mr-2" @click="editItem(item)"> mdi-pencil </v-icon>
         <v-icon small @click="deleteItem(item)"> mdi-delete </v-icon>
