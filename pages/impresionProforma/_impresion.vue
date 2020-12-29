9dd3273b81bb2f4fef83a3eb26262347254900c0
francocava <francocavallini@hotmail.com>
2020-12-29 18:41:40 -0300

    avances varios de impresion

--- pages/impresionProforma/_impresion.vue
+++ pages/impresionProforma/_impresion.vue
@@ -1,17 +1,14 @@
 <template>
   <div>
-    <v-container class="mx-auto my-12">
+    <v-container class="mx-auto my-12" v-if="pedido">
       <v-row align="center" no-gutters>
         <v-col>
           <v-card class="pa-2" outlined tile align="center">
-            <v-img
-              src="https://i.imgur.com/RFwozBU.jpg"
-              max-height="111"
-              max-width="309"
-            ></v-img>
-            <v-card-text>Av. Rivadavia 8860</v-card-text>
-            <v-card-text>Tel: 4672-9996</v-card-text>
-            <v-card-text>IVA Responsable Inscripto</v-card-text>
+            <v-img src="/img/logo.jpg" max-height="111" max-width="309"></v-img>
+            <v-card-text> <div class="red--text"><span class="red--text">Av. Rivadavia 8860</span></div>
+             <div>Tel: 4672-9996</div>
+             <div>IVA Responsable Inscripto</div>
+            </v-card-text>
           </v-card>
         </v-col>
         <v-col>
@@ -27,22 +24,20 @@
       <v-row no-gutters>
         <v-col>
           <v-card outlined style="height: 150px">
-            <!--             <v-card-text>
+            <v-card-text>
               Cliente: {{ pedido.cliente.nombre }} {{ pedido.cliente.apellido }}
             </v-card-text>
             <v-card-text>
               Obra Social: {{ pedido.cliente.obra_social.nombre }}
-            </v-card-text> -->
+            </v-card-text>
           </v-card>
         </v-col>
         <v-col>
           <v-card outlined style="height: 150px">
-            <!--             <v-card-text>
+            <v-card-text>
               Afiliado: {{ pedido.cliente.nro_afiliado }}
             </v-card-text>
-            <v-card-text>
-              Teléfono: {{ pedido.cliente.telefono }}
-            </v-card-text> -->
+            <v-card-text> Teléfono: {{ pedido.cliente.telefono }} </v-card-text>
           </v-card>
         </v-col>
       </v-row>
@@ -53,7 +48,7 @@
               <template v-slot:default>
                 <thead>
                   <tr>
-                    <th class="text-left">Nombre</th>
+                    <th class="text-left font-weight-black">Nombre</th>
                     <th class="text-left">Descripción</th>
                     <th class="text-left">Precio</th>
                   </tr>
@@ -83,15 +78,15 @@
         </v-col>
       </v-row>
       <v-row>
+<!--         <v-spacer></v-spacer>
         <v-spacer></v-spacer>
         <v-spacer></v-spacer>
         <v-spacer></v-spacer>
-        <v-spacer></v-spacer>
-        <v-spacer></v-spacer>
+        <v-spacer></v-spacer> -->
         <v-col>
-          <v-card outlined min-height="50px" width="200px"  align="center">
+          <v-card flat align="end" class="d-flex flex-row-reverse">
             <v-card-text> Total: ${{ pedido.importe }} </v-card-text>
-            </v-card>
+          </v-card>
         </v-col>
       </v-row>
       <v-row>
@@ -115,9 +110,11 @@
       <v-row>
         <v-col>
           <v-card outlined style="height: 150px">
-          <v-card-title>Condiciones Comerciales</v-card-title>
-          <v-card-text>- Condición de Pago: Pago previo a entrega</v-card-text>
-          <v-card-text>- Plazo de entrega: 30 días</v-card-text>
+            <v-card-title>Condiciones Comerciales</v-card-title>
+            <v-card-text
+              >- Condición de Pago: Pago previo a entrega</v-card-text
+            >
+            <v-card-text>- Plazo de entrega: 30 días</v-card-text>
           </v-card>
         </v-col>
       </v-row>
@@ -129,7 +126,7 @@
 export default {
   layout: 'empty',
   data: () => ({
-    pedido: [],
+    pedido: '',
     alignments: ['start', 'center', 'end'],
   }),
 
@@ -141,12 +138,6 @@ export default {
 
     this.productos = await this.$http.$get('producto')
     this.accesorios = await this.$http.$get('accesorio')
-  },
-
-  method: {
-    initialize() {
-      this.$fetch()
-    },
   },
 }
 </script>
