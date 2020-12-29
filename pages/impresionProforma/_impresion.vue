b308c40b626f5a7d12f95464be52d816360201f5
Agustin Amato <agustin.amato96@gmail.com>
2020-12-29 19:23:04 -0300

    impresion: ajuste tipografia y espaciado

--- pages/impresionProforma/_impresion.vue
+++ pages/impresionProforma/_impresion.vue
@@ -1,49 +1,51 @@
 <template>
   <div>
     <v-container class="mx-auto my-12" v-if="pedido">
-      <v-row align="center" no-gutters>
+      <v-row align="center">
         <v-col>
           <v-card class="pa-2" outlined tile align="center">
             <v-img src="/img/logo.jpg" max-height="111" max-width="309"></v-img>
-            <v-card-text> <div class="red--text"><span class="red--text">Av. Rivadavia 8860</span></div>
-             <div>Tel: 4672-9996</div>
-             <div>IVA Responsable Inscripto</div>
+            <v-card-text class="black--text">
+              <div>Av. Rivadavia 8860</div>
+              <div>Tel: 4672-9996</div>
+              <div>IVA Responsable Inscripto</div>
             </v-card-text>
           </v-card>
         </v-col>
         <v-col>
-          <v-card class="pa-2" outlined tile align="center">
-            <v-card-text> Nro Cotización: {{ pedido.id }} </v-card-text>
-            <v-card-text>
-              Fecha: {{ pedido.fecha_ingreso_autorizacion }}
+          <v-card class="pa-2 h-100" outlined tile align="center">
+            <v-card-text class="black--text">
+              <div>Nro Cotización: {{ pedido.id }}</div>
+              <div>Fecha: {{ pedido.fecha_ingreso_autorizacion }}</div>
+              <div>CUIT Nro: 20-13515459-9</div>
             </v-card-text>
-            <v-card-text>CUIT Nro: 20-13515459-9</v-card-text>
-          </v-card>
-        </v-col>
-      </v-row>
-      <v-row no-gutters>
-        <v-col>
-          <v-card outlined style="height: 150px">
-            <v-card-text>
-              Cliente: {{ pedido.cliente.nombre }} {{ pedido.cliente.apellido }}
-            </v-card-text>
-            <v-card-text>
-              Obra Social: {{ pedido.cliente.obra_social.nombre }}
-            </v-card-text>
-          </v-card>
-        </v-col>
-        <v-col>
-          <v-card outlined style="height: 150px">
-            <v-card-text>
-              Afiliado: {{ pedido.cliente.nro_afiliado }}
-            </v-card-text>
-            <v-card-text> Teléfono: {{ pedido.cliente.telefono }} </v-card-text>
           </v-card>
         </v-col>
       </v-row>
       <v-row>
         <v-col>
-          <v-card flat>
+          <v-card outlined align="center">
+            <v-card-text class="black--text">
+              <div>
+                Cliente: {{ pedido.cliente.nombre }}
+                {{ pedido.cliente.apellido }}
+              </div>
+              <div>Obra Social: {{ pedido.cliente.obra_social.nombre }}</div>
+            </v-card-text>
+          </v-card>
+        </v-col>
+        <v-col>
+          <v-card outlined align="center">
+            <v-card-text class="black--text">
+              <div>Afiliado: {{ pedido.cliente.nro_afiliado }}</div>
+              <div>Teléfono: {{ pedido.cliente.telefono }}</div>
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <v-card outlined>
             <v-simple-table fixed-header>
               <template v-slot:default>
                 <thead>
@@ -78,20 +80,15 @@
         </v-col>
       </v-row>
       <v-row>
-<!--         <v-spacer></v-spacer>
-        <v-spacer></v-spacer>
-        <v-spacer></v-spacer>
-        <v-spacer></v-spacer>
-        <v-spacer></v-spacer> -->
         <v-col>
-          <v-card flat align="end" class="d-flex flex-row-reverse">
-            <v-card-text> Total: ${{ pedido.importe }} </v-card-text>
+          <v-card outlined align="end" class="d-flex flex-row-reverse">
+            <v-card-text class="black--text"> Total: ${{ pedido.importe }} </v-card-text>
           </v-card>
         </v-col>
       </v-row>
       <v-row>
         <v-col>
-          <v-card flat>
+          <v-card outlined>
             <v-card-text
               >IMPORTANTE: Ante una repentina devaluación de la moneda los
               precios pueden verse modificados sin mediar preaviso
@@ -99,9 +96,9 @@
           </v-card>
         </v-col>
       </v-row>
-      <v-row>
+      <v-row v-if="pedido.observaciones">
         <v-col>
-          <v-card outlined style="height: 100px">
+          <v-card outlined>
             <v-card-title>Observaciones</v-card-title>
             <v-card-text>{{ pedido.observaciones }}</v-card-text>
           </v-card>
@@ -109,12 +106,12 @@
       </v-row>
       <v-row>
         <v-col>
-          <v-card outlined style="height: 150px">
+          <v-card outlined>
             <v-card-title>Condiciones Comerciales</v-card-title>
-            <v-card-text
-              >- Condición de Pago: Pago previo a entrega</v-card-text
-            >
-            <v-card-text>- Plazo de entrega: 30 días</v-card-text>
+            <v-card-text>
+              <div>- Condición de Pago: Pago previo a entrega</div>
+              <div>- Plazo de entrega: 30 días</div>
+            </v-card-text>
           </v-card>
         </v-col>
       </v-row>
@@ -125,19 +122,41 @@
 <script>
 export default {
   layout: 'empty',
+
   data: () => ({
     pedido: '',
-    alignments: ['start', 'center', 'end'],
   }),
 
   async fetch() {
-    console.log(this.$route.params.impresion)
-    const rsp = await this.$http.$get(`pedido/${this.$route.params.impresion}`)
-    this.pedido = rsp
-    console.log(this.pedido)
+    try {
+      this.pedido = await this.$http.$get(
+        `pedido/${this.$route.params.impresion}`
+      )
 
-    this.productos = await this.$http.$get('producto')
-    this.accesorios = await this.$http.$get('accesorio')
+      this.productos = await this.$http.$get('producto')
+      this.accesorios = await this.$http.$get('accesorio')
+    } catch (error) {
+      console.error(error.response ?? error)
+    }
   },
 }
 </script>
+
+<style>
+.black--text {
+  color: #000 !important;
+}
+.h-100 {
+  height: 100%;
+}
+.col {
+  padding: 2px;
+}
+.v-sheet--outlined {
+    border: thin solid rgba(0, 0, 0, 0.7) !important;
+}
+th {
+  font-size: 1rem !important;
+  text-align: left !important;
+}
+</style>
