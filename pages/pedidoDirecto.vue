49d62f54128def638c8e80d5496de99eeafc2216
francocava <francocavallini@hotmail.com>
2021-01-04 20:46:04 -0300

    ahora se pueden poner cantidades de producto

--- pages/pedidoDirecto.vue
+++ pages/pedidoDirecto.vue
@@ -9,7 +9,6 @@
 
       <v-card-text>
         <v-form ref="form" v-model="valid" lazy-validation>
-
           <v-combobox
             v-model="form.cliente"
             :items="clientes"
@@ -24,7 +23,9 @@
               <v-list-item>
                 <v-list-item-content>
                   <v-list-item-title>
-                    No se encuentra al cliente "<strong>{{ searchCliente }}</strong
+                    No se encuentra al cliente "<strong>{{
+                      searchCliente
+                    }}</strong
                     >".
                   </v-list-item-title>
                 </v-list-item-content>
@@ -42,16 +43,38 @@
             multiple
             :search-input.sync="searchProducto"
             chips
+            :loading="loading"
           >
             <template v-slot:no-data>
               <v-list-item>
                 <v-list-item-content>
                   <v-list-item-title>
-                    No se encuentra al producto "<strong>{{ searchProducto }}</strong
+                    No se encuentra al producto "<strong>{{
+                      searchProducto
+                    }}</strong
                     >".
                   </v-list-item-title>
                 </v-list-item-content>
               </v-list-item>
+            </template>
+
+            <template
+              v-slot:selection="{ attrs, item, parent, selected, index }"
+            >
+              <v-chip
+                v-if="item === Object(item)"
+                v-bind="attrs"
+                :color="`${item.color} lighten-3`"
+                :input-value="selected"
+                label
+                small
+              >
+                <span class="pr-2">
+                  {{ item.nombre }} {{ item.cantidad }}
+                </span>
+                <v-icon small @click="remove(index, item)"> mdi-minus </v-icon>
+                <v-icon small @click="add(index, item)"> mdi-plus </v-icon>
+              </v-chip>
             </template>
           </v-combobox>
 
@@ -70,7 +93,9 @@
               <v-list-item>
                 <v-list-item-content>
                   <v-list-item-title>
-                    No se encuentra al accesorio "<strong>{{ searchAccesorio }}</strong
+                    No se encuentra al accesorio "<strong>{{
+                      searchAccesorio
+                    }}</strong
                     >".
                   </v-list-item-title>
                 </v-list-item-content>
@@ -104,6 +129,7 @@
           </v-btn>
         </v-form>
       </v-card-text>
+      <!-- <v-card-text>Total {{ form.productos[0].precio}}</v-card-text> -->
     </v-card>
     <v-snackbar v-model="snackbar.display" :color="snackbar.color">
       {{ snackbar.text }}
@@ -118,50 +144,55 @@ export default {
     this.sucursales = await this.$http.$get('sucursal')
     this.clientes = await this.$http.$get('cliente')
     this.productos = await this.$http.$get('producto')
+
+    this.loading = false
   },
 
-  data: () => ({
-    snackbar: {
-      display: false,
-      text: '',
-      color: 'black',
-    },
+  data() {
+    return {
+      loading: true,
 
-    form: {
-      fecha_ingreso_autorizacion: new Date().toISOString().substr(0, 10),
-      fecha_retiro: new Date().toISOString().substr(0, 10),
+      snackbar: {
+        display: false,
+        text: '',
+        color: 'black',
+      },
+
+      form: {
+        fecha_ingreso_autorizacion: new Date().toISOString().substr(0, 10),
+        fecha_retiro: new Date().toISOString().substr(0, 10),
+        productos: [],
+        accesorios: [],
+        cliente: null,
+        sucursal_id: null,
+        observaciones: null,
+        confirmado: true,
+        usuario_id: 1, //pongo 1 por defecto pero tendria que sacar este dato de la sesion
+        cantidad: [],
+      },
+      valid: true,
+      menuAutorizacion: false,
+      menuRetiro: false,
+      searchCliente: null,
+      searchProducto: null,
+      searchAccesorio: null,
+
+      nombreRules: [
+        (v) => !!v || 'Falta el nombre del cliente',
+        (v) => (v && v.length <= 15) || 'Nombre muy largo',
+      ],
+      apellido: '',
+      apellidoRules: [
+        (v) => !!v || 'Falta el apellido del cliente',
+        (v) => (v && v.length <= 25) || 'Apellido muy largo',
+      ],
+      select: null,
+      clientes: [],
       productos: [],
       accesorios: [],
-      cliente: null,
-      sucursal_id: null,
-      observaciones: null,
-      confirmado: true,
-      usuario_id: 1, //pongo 1 por defecto pero tendria que sacar este dato de la sesion
-      //fac_id
-      //nro_recibo_proveedor
-    },
-    valid: true,
-    menuAutorizacion: false,
-    menuRetiro: false,
-    searchCliente: null,
-    searchProducto: null,
-    searchAccesorio: null,
-
-    nombreRules: [
-      (v) => !!v || 'Falta el nombre del cliente',
-      (v) => (v && v.length <= 15) || 'Nombre muy largo',
-    ],
-    apellido: '',
-    apellidoRules: [
-      (v) => !!v || 'Falta el apellido del cliente',
-      (v) => (v && v.length <= 25) || 'Apellido muy largo',
-    ],
-    select: null,
-    clientes: [],
-    productos: [],
-    accesorios: [],
-    sucursales: [],
-  }),
+      sucursales: [],
+    }
+  },
 
   methods: {
     async validate() {
@@ -177,6 +208,16 @@ export default {
         this.showSnackbar(`Ocurrió un error: ${error.message}`, 'red')
       }
     },
+
+    add(index, item) {
+      this.form.productos[index].cantidad++
+    },
+
+    remove(index, item) {
+      if (this.form.productos[index].cantidad > 1)
+        this.form.productos[index].cantidad--
+    },
+
     showSnackbar(message, color) {
       this.snackbar.display = true
       this.snackbar.text = message
@@ -184,5 +225,4 @@ export default {
     },
   },
 }
-
 </script>
