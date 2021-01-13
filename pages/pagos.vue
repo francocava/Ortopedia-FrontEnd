52acc0693ec73751fea71844e85f7d42356954b4
francocava <francocavallini@hotmail.com>
2021-01-13 19:00:45 -0300

    pagos fix

--- pages/pagos.vue
+++ pages/pagos.vue
@@ -100,7 +100,7 @@
           <v-dialog v-model="dialogDelete" max-width="500px">
             <v-card>
               <v-card-title class="headline"
-                >Estas seguro que queres eliminar el item?</v-card-title
+                >Estás seguro que queres eliminar el pago?</v-card-title
               >
               <v-card-actions>
                 <v-spacer></v-spacer>
